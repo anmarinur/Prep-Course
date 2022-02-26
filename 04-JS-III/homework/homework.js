b67bf3c222b1165df0aa5006d9d7d555d46962b1
anmarinur <andersonmarindev@gmail.com>
2022-02-25 22:38:18 -0500

    Hasta el punto agregarNumeros

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,21 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length - 1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 
@@ -23,6 +26,10 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for (var i = 0; i < array.length; i++){
+    array[i]++;
+  }
+  return array;
 }
 
 
@@ -30,6 +37,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
 }
 
 
@@ -38,6 +47,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array;
 }
 
 
@@ -47,6 +58,23 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  var sum = [];
+  var frase = 0;
+  for (var i = 0; i < palabras.length; i++){
+    if (i == palabras.length - 1){
+      sum[i] = palabras[i];
+    } else {
+      sum[i] = palabras[i] + ' ';
+    }
+  }
+  for (var j = 0; j < sum.length; j++){
+    if (j == 0){
+      frase = sum[j];
+    } else {
+      frase = frase + sum[j];
+    }
+  }
+  return frase;
 }
 
 
@@ -54,6 +82,18 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  var cont = 0;
+  for (var i = 0; i < array.length; i++){
+    if (array[i] == elemento){
+      cont++;
+    }
+  }
+  if (cont >= 1){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 
